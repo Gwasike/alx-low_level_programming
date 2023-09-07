0f5bffd0c423f920330e8d5a64c0bb21c42d2b8c
Gwasike <wasikegrace177@gmail.com>
2023-09-07 15:03:41 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: 0 (Sucess)
+ * Return:Always 0 (Sucess)
  */
 int main(void)
 {	 
-	 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	 return (1);
 }	 
