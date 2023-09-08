602188f0e22621c78d336f1c2ab53556dbfc6151
Gwasike <wasikegrace177@gmail.com>
2023-09-08 14:54:03 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	        char d;
+
+		d = 'a';
+		while
+			(d <= 'z') {
+				if ((d != 'q' && d != 'e') && d <= 'z')
+					putchar(d);
+				d++;
+			}
+		putchar('\n');
+		return (0);
+}
+
+
+
