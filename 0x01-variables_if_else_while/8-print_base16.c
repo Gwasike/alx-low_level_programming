93faeae8b03b30c5ea2997de125f3cb44f35b6db
Gwasike <wasikegrace177@gmail.com>
2023-09-08 15:39:11 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d < 10) {
+			putchard(d + '0');
+			d++
+		}
+	while
+		(c <= 'f') {
+			putchard(c);
+			c++;
+		}
+	putchard('\n');
+	return (0);
+}
