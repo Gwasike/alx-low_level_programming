e6fae739e3794027c133ca65e10cde0edac8b685
Gwasike <wasikegrace177@gmail.com>
2023-09-08 14:25:51 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char j;
+
+	char k;
+
+	j = 'a';
+	k = 'A';
+	while
+		(j <= 'z') {
+			putchar(j);
+			j++;
+		}
+	while
+		(k <= 'z') {
+			putchar (k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
