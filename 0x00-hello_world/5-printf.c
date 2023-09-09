d799fc672183a76871e7201f0c405eb4b80242f1
Gwasike <wasikegrace177@gmail.com>
2023-09-09 02:39:14 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
  * main -this is a printf function
  * Return: 0
  */
-init main(void)
+
+int main(void)
 {
 	 printf("with proper grammar, but the outcome is a piece of art,\n");
-         return (0);
-}	 
+	 return (0);
+}
 	
