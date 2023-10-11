1a072e8ede802211187aae52a8789721a52f4d6d
Gwasike <wasikegrace177@gmail.com>
2023-10-11 10:59:08 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that executes a function
+ * @array: array
+ * @size: size
+ * @action: action
+ *
+ * Return: none
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int k = 0;
+
+	if (array != NULL && action !=NULL && size > 0)
+	{
+		while (k < size)
+		{
+			action(array[k]);
+			k++;
+		}
+	}
+}
