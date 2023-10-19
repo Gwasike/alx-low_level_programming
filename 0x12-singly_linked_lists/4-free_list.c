39c240895a86a2e1218b704303dee8ecd3e95f03
Gwasike <wasikegrace177@gmail.com>
2023-10-19 14:26:51 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_list - function that frees 
+ * @head: ...
+ * Return: none
+ */
+
+void free_list(list_t *head)
+{
+	list_t *present_node;
+
+	while ((present_node = head) != NULL)
+	{
+		head = head->next;
+		free(present_node->str);
+		free(present_node);
+	}
+}
