36bcb1b414aafcb44b2243b94b541d6a806c4ac9
Gwasike <wasikegrace177@gmail.com>
2023-10-24 09:20:36 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: pointer to the address
+ *
+ * Description: NULL
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		node = (*head)->next;
+		free(*head);
+		*head = node;
+	}
+
+	head = NULL;
+}
