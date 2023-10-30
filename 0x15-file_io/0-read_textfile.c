0aeb34c31bab68b431ac48f33650bc3901273384
Gwasike <wasikegrace177@gmail.com>
2023-10-30 10:41:47 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,31 +13,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_d;
-	ssize_t lenr, lenw;
-	char *buffer;
+	char *buff;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	file_d = open(filename, O_RDONLY);
-	if (file_d == -1)
-		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(file_d);
-		return (0);
-	}
-	lenr = read(file_d, buffer, letters);
-	close(file_d);
-	if (lenr == -1)
-	{
-		free(buffer);
-		return (0);
-	}
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free(buffer);
-	if (lenr != lenw)
-		return (0);
-	return (lenw);
+	buff = malloc(sizeof(char) * letters);
+	t = read(fd, buff, letters);
+	w = write(STDOUT_FILENO, buff, t);
+
+	free(buff);
+	close(fd);
+	return (w);
 }
