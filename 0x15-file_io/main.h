378008306dbb29230f064a41886add2342d4f81c
Gwasike <wasikegrace177@gmail.com>
2023-10-30 08:55:18 -0700

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,8 @@
-#ifndef _MAIN.H_
-#define _MAIN.H_
+#ifndef _MAIN_
+#define _MAIN_
+#include <stddef.h>
 
-int _putchar.c(char c);
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
