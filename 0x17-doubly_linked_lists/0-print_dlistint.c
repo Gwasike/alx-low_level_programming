5d655b7c17dea0d530593f2a0b12f963aa80456d
Gwasike <wasikegrace177@gmail.com>
2023-12-07 07:12:14 -0800

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements
+ * @h: head node
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t k = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		k++;
+	}
+	return (k);
+}
